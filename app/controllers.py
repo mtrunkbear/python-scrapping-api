b225a2a583a332b495d4ec1e26a6f2ac2415cb2f
Miguel Troncoso <96815554+mtrunkbear@users.noreply.github.com>
2023-07-04 20:34:26 -0400

    Refactor controllers.py

--- app/controllers.py
+++ app/controllers.py
@@ -9,14 +9,14 @@ absolute_path = os.path.dirname(__file__)
 
 @application.route('/api/product/<product>', methods=['GET'])
 def get_product_list(product):
-    product_clases = {"microwaves": {"url": "https://www.coolblue.nl/en/microwaves/freestanding/solo-microwaves"},
+    product_classes = {"microwaves": {"url": "https://www.coolblue.nl/en/microwaves/freestanding/solo-microwaves"},
                       "washers&dryers": {"url": "http://www.coolblue.nl/en/washer-dryer-combos/filter"}}
     data_path = "./storage/"+product+".json"
     if os.path.exists(data_path):
         with open(data_path, "r") as json_file:
             data = json.load(json_file)
     else:
-        data = get_products(product_clases[product]["url"])
+        data = get_products(product_classes[product]["url"])
         write_data = json.dumps(data)
         with open(data_path, "w") as outfile:
             outfile.write(write_data)
